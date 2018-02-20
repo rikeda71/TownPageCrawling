6a3a114bc7feac5f3955a08754fd3b0ce6dfebbb
Ryuya Ikeda <rikeda71@gmail.com>
2018-02-20 11:43:40 +0900

    Fix: refactoring

--- saveshopinfo.py
+++ saveshopinfo.py
@@ -7,7 +7,7 @@ import pandas as pd
 def extract_infomation(text: str=""):
     """
     HTMLから書かれている情報を抽出する
-    店名，URL，メールアドレス，郵便番号，住所，電話番号
+    店名，郵便番号，住所，電話番号, URL, メールアドレス
     text : HTMLの内容
     """
 
@@ -20,7 +20,7 @@ def extract_infomation(text: str=""):
     telnumbers = []
     idx = 0
 
-    # URL, e-mail, 郵便番号，住所，電話番号を保存
+    # 郵便番号，住所，電話番号, URL, e-mailを保存
     for s in soup.find_all("dl")[10:]:
         details = s.find_all("dt")
         info = s.find_all("dd")
@@ -30,14 +30,8 @@ def extract_infomation(text: str=""):
         addresses.append("")
         telnumbers.append("")
         for i in range(len(details)):
-            # お店のURLを保存
-            if str(details[i]) == "<dt>【URL】</dt>":
-                urls[idx] = info[i].text
-            # メールアドレスを保存
-            elif str(details[i]) == "<dt>【e-mail】</dt>":
-                emails[idx] = info[i].text
             # 郵便番号と住所を保存
-            elif str(details[i]) == "<dt>【住所】</dt>":
+            if str(details[i]) == "<dt>【住所】</dt>":
                 # 郵便番号と住所を分割し保存
                 codes = info[i].text.split(" ")
                 postalcodes[idx] = codes[0]
@@ -45,8 +39,14 @@ def extract_infomation(text: str=""):
             # 電話番号を保存
             elif str(details[i]) == "<dt>【電話番号】</dt>":
                 telnumbers[idx] = info[i].text
+            # お店のURLを保存
+            elif str(details[i]) == "<dt>【URL】</dt>":
+                urls[idx] = info[i].text
+            # メールアドレスを保存
+            elif str(details[i]) == "<dt>【e-mail】</dt>":
+                emails[idx] = info[i].text
         idx += 1
-    return shops, urls, emails, postalcodes, addresses, telnumbers
+    return shops, postalcodes, addresses, telnumbers, urls, emails
 
 
 def pref_save_shopinfo(path: str):
@@ -70,21 +70,21 @@ def pref_save_shopinfo(path: str):
             text = f.read()
         info = extract_infomation(text)
         shops.extend(info[0])
-        urls.extend(info[1])
-        emails.extend(info[2])
-        postalcodes.extend(info[3])
-        addresses.extend(info[4])
-        telnumbers.extend(info[5])
+        postalcodes.extend(info[1])
+        addresses.extend(info[2])
+        telnumbers.extend(info[3])
+        urls.extend(info[4])
+        emails.extend(info[5])
 
     # pandasの形式に変換してcsvに保存
-    for (s, u, e, p, a, t) in zip(shops, urls, emails, postalcodes, addresses, telnumbers):
-        data.append([s, u, e, p, a, t])
+    for (s, p, a, t, u, e) in zip(shops, postalcodes, addresses, telnumbers, urls, emails):
+        data.append([s, p, a, t, u, e])
     df = pd.DataFrame(data,
-                      columns=["name", "url", "email", "postalcode", "address", "telnumber"])
+                      columns=["name", "postalcode", "address", "telnumber", "url", "email"])
     df.to_csv(directory + "/" + path[path.index("/"):] + ".csv", index=False)
 
 
-def all_save_shopinfo(path: str):
+def all_save_shopinfo(paths: str):
     """
     カテゴリ内の情報を保存する
     paths : カテゴリのディレクトリ
@@ -93,7 +93,7 @@ def all_save_shopinfo(path: str):
     # 情報を保持するディレクトリを作成
     make_dir(directory)
     # 都道府県ごとに情報を保存
-    for prefdir in sort_glob(path + "/*"):
+    for prefdir in sort_glob(paths + "/*"):
         pref_save_shopinfo(prefdir)
 
 
